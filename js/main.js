696c0f00f66da900e94e5f35f1c175b79f353ee1
Lova Wins <lovaunger@gmail.com>
2019-11-11 15:10:59 +0100

    added sort function
    la till funktion för att sortera bilderna. och layout/design med bootstrap

--- js/main.js
+++ js/main.js
@@ -1,37 +1,51 @@
 /* webappliaktion som använder flickr api för att hämta bilder av Lova Unger 2019 */
 
 
-
-
-
-
-
  document.querySelector('button').addEventListener('click', async () => {
 
-let text = document.querySelector('#search').value;
+let input = document.querySelector('#search').value;
+let sort = document.querySelector('input[name="sortby"]:checked').value;
+
+
    
 // Get images 
-let data = await getImages(text);
+let data = await getImages(input,sort);
 //update UI 
 updateUI(data);
+
+  // Get sort 
+
+  console.log(sort);
+  
 
 
  })
 
- async function getImages(text){
+
+ 
+ async function getImages(input,sort){
    const apiKey = '6765cdd05a0a90926774a7abe7ccef23';
    const baseUrl =  'https://api.flickr.com/services/rest';
-   let method = 'flickr.photos.search'
    
-   let url = `${baseUrl}?api_key=${apiKey}&method=${method}&text=${text}&format=json&nojsoncallback=1`;
-    
+   let method = 'flickr.photos.search';
+   let url = `${baseUrl}?api_key=${apiKey}&method=${method}&text=${input}&sort=${sort}&format=json&nojsoncallback=1`;
    let resp = await fetch(url);
    let data = await resp.json();
    console.log(data);
 
+
+ 
+
+
    return data.photos;
+
+   
    
  }
+
+
+
+
 // update UI 
 
 document.querySelector('#photos').innerHTML = "";
@@ -41,7 +55,11 @@ document.querySelector('#photos').innerHTML = "";
        let elImg = document.createElement('img');
        elImg.setAttribute('src', imgUrl(img, 'q'));
        document.querySelector('#photos').appendChild(elImg);
+     
+
     });
+
+
  }
 
  // construct img url
